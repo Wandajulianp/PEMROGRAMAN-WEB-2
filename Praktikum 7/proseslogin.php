53b42398717d1000fae01aa4b683b86a74e56944
Wandajulianp <wandajulianprasetya1@gmail.com>
2025-11-27 16:53:53 +0700

    Create proseslogin.php

---
+++ Praktikum 7/proseslogin.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Login</title>
+</head>
+<body>
+
+<h2>Login User</h2>
+<form action="login_proses.php" method="POST">
+    Username : <input type="text" name="username" required><br><br>
+    Password : <input type="password" name="password" required><br><br>
+
+    <button type="submit">Login</button>
+</form>
+
+<br>
+<a href="register.php">Belum punya akun? Daftar disini</a>
+
+</body>
+</html>
